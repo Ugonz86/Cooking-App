00d215b27a7fa010330d0d9a26b6b0eae14fa2ec
Jordan Safford and Uriel Gonzalez <uriel@>
2019-10-21 13:51:39 -0700

    add oz to lbs coversion

--- JS/scripts.js
+++ JS/scripts.js
@@ -4,4 +4,12 @@ var conversion = function(liquid) {
 
 var liquid = parseFloat(prompt("Enter the measurement in gallons: "));
 var result = conversion(liquid);
-alert(("One gallon equals ") + result.toFixed(1) + " liters!");
+alert(("It equals ") + result.toFixed(1) + " liters!");
+
+var conversionTwo = function(weight) {
+  return (weight / 16);
+};
+
+var weight = parseFloat(prompt("Enter the weight in ounces"));
+var result = conversionTwo(weight);
+alert(("It weighs ") + result.toFixed(1) + " pounds!");
